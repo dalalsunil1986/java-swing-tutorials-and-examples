e0850ca9a93b2af7c8fb84620cc9a43f2cecf856
soumyadip007 <soumyadip.note@gmail.com>
2019-06-26 16:32:55 +0530

    Menu

--- Java-Swing/src/net/javaguides/javaswing/examples/Menu.java
+++ Java-Swing/src/net/javaguides/javaswing/examples/Menu.java
@@ -4,4 +4,50 @@ import javax.swing.*;
 
 class MenuBar extends JFrame{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	MenuBar(){}
+	MenuBar(String s){super(s);}
+	
+	JMenuBar mb;
+	JMenu Menu,SubMenu;
+	JMenuItem Item1,Item2,Item3,Item4,Item5;
+	void Menu1(){
+		mb=new JMenuBar();
+		
+		Menu=new JMenu("Menu");
+		SubMenu=new JMenu("Sub-Menu");
+				
+		Item1=new JMenuItem("Item1");		
+		Item2=new JMenuItem("Item2");
+		Item3=new JMenuItem("Item3");		
+		Item4=new JMenuItem("Item4");
+		Item5=new JMenuItem("Item5");
+		
+		Menu.add(Item1);
+		Menu.add(Item2);
+		Menu.add(Item3);
+		
+		SubMenu.add(Item4);
+		SubMenu.add(Item5);
+		Menu.add(SubMenu);
+		
+		mb.add(Menu);
+		//mb.add(SubMenu);
+		
+		setJMenuBar(mb);
+	}
+}
+public class Menu extends MenuBar{
+
+	public static void main(String args[])
+	{
+		MenuBar jf=new MenuBar("Menu");
+		jf.Menu1();
+		jf.setLayout(null);
+		jf.setSize(400,300);
+		jf.setVisible(true);
+}
 }
